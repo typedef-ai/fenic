282c74834196af0347e1ec114b1891841df3e130
David Youngworth <david@typedef.ai>
2025-09-02 11:21:10 -0700

    use gpt-5-nano

--- tests/conftest.py
+++ tests/conftest.py
@@ -126,7 +126,7 @@ def pytest_addoption(parser):
     parser.addoption(
         LANGUAGE_MODEL_NAME_ARG,
         action="store",
-        default="gpt-4.1-nano",
+        default="gpt-5-nano",
         help="Model Name to run tests against",
     )
     parser.addoption(
@@ -188,7 +188,7 @@ def multi_model_local_session_config(app_name, request) -> SessionConfig:
         language_models = {
             "model_1": nano,
             "model_2": OpenAILanguageModel(
-                model_name="gpt-4.1-mini", rpm=250, tpm=50_000,
+                model_name="gpt-5-nano", rpm=250, tpm=50_000,
             ),
         }
     elif language_model_provider == ModelProvider.ANTHROPIC:
